64e789104d7dd0ec37c51dc32a3292626a5ed150
vood <balitsky_igor@list.ru>
2019-09-21 14:54:14 +0300

    ant

--- src/main/java/ru/sberbank/inkass/Application.java
+++ src/main/java/ru/sberbank/inkass/Application.java
@@ -1,18 +1,30 @@
 package ru.sberbank.inkass;
 
 import org.apache.commons.lang3.tuple.Pair;
+import ru.sberbank.inkass.calc.AntWayDto;
 import ru.sberbank.inkass.dto.PointDto;
 import ru.sberbank.inkass.fill.FillGraphService;
 import ru.sberbank.inkass.fill.FillGraphServiceImpl;
 import ru.sberbank.inkass.fill.WayInfoDto;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
 
 public class Application {
 
-    public static int MAX_SUM_IN_POINT = 10000;
-    public static int MAX_TIME_IN_POINT = 200;
-    public static int MAX_TIME_IN_WAY = 2000;
+
+    public static final int WORKING_DAY_COUNT = 100;
+    public static final int ANT_COUNT = 50_000;
+    public static final int MAX_SUM_IN_POINT = 10_000;
+    public static final int MAX_TIME_IN_POINT = 200;
+    public static final int MAX_TIME_IN_WAY = 2_000;
+    public static final int MAX_MONAY_IN_ANT = MAX_SUM_IN_POINT * 5;
+    public static final int WORKIMG_DAY_LENGTH = 20_000;
 
 
     public static void main(String[] args) {
@@ -21,6 +33,22 @@ public class Application {
 
         fill.forEach((k, v) -> System.out.println(k + v.toString()));
         System.out.println(fill.size());
+
+        System.out.println(new Date() + "++++++++++++++++++++++++");
+        final List<AntWayDto> collect = IntStream.range(0, ANT_COUNT)
+                .parallel()
+                .mapToObj(i -> new AntWayDto(fill))
+                .collect(toList());
+        System.out.println(new Date() + "++++++++++++++++++++++++" + collect.size());
+
+        System.out.println(new Date() + "=====================");
+        List<AntWayDto> antWayDtos = new ArrayList<>();
+        for (int i = 0; i < ANT_COUNT; i++) {
+            antWayDtos.add(new AntWayDto(fill));
+        }
+        AntWayDto antWayDto = new AntWayDto(fill);
+        System.out.println(new Date() + "=====================" + antWayDtos.size());
+        System.out.println(antWayDto);
 
     }
 }
--- src/main/java/ru/sberbank/inkass/calc/AntWayDto.java
+++ src/main/java/ru/sberbank/inkass/calc/AntWayDto.java
@@ -1,5 +1,7 @@
 package ru.sberbank.inkass.calc;
 
+import lombok.Getter;
+import lombok.ToString;
 import org.apache.commons.lang3.tuple.Pair;
 import ru.sberbank.inkass.dto.PointDto;
 import ru.sberbank.inkass.fill.WayInfoDto;
@@ -7,25 +9,32 @@ import ru.sberbank.inkass.fill.WayInfoDto;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toMap;
 
+@Getter
+@ToString
 public class AntWayDto {
 
     private Long totalTime;
     private Long totalMoney;
     private Long moneyOnThisTrip;
     private List<PointDto> way;
-    private List<PointDto> notVisitedPoint;
+    private Set<PointDto> notVisitedPoint;
     private Map<Pair<PointDto, PointDto>, WayInfoDto> roadMap;
 
 
-    public AntWayDto(Map<Pair<PointDto, PointDto>, WayInfoDto> roadMap, List<PointDto> notVisitedPoint) {
+    public AntWayDto(Map<Pair<PointDto, PointDto>, WayInfoDto> roadMap) {
         this.totalTime = 0L;
         this.totalMoney = 0L;
         this.moneyOnThisTrip = 0L;
         this.way = new ArrayList<>();
-        this.notVisitedPoint = notVisitedPoint;
+        this.notVisitedPoint =
+                roadMap.keySet().stream()
+                        .map(Pair::getKey)
+                        .collect(Collectors.toSet());
         this.roadMap =
                 roadMap.entrySet().stream()
                         .map(e -> Pair.of(
